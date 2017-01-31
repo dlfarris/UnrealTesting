3ff85f8e8cfe964d56951316d9637044c5399467
Dustin Farris <dfarris88@gmail.com>
2017-01-30 23:00:23 -0800

    lecture 66 Triggers

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -20,11 +20,16 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+
 	// Find the owning Actor
 	AActor* owner = GetOwner();
-	FRotator rotation = FRotator(0.0f, 90.0f, 0.0f);
+	FRotator rotation = FRotator(0.0f, 75.0f, 0.0f);
 	owner->SetActorRotation(rotation);
-	
 }
 
 
@@ -33,6 +38,10 @@ void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompo
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	// Poll the trigger volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) 
+	{
+		OpenDoor();
+	}
 }
 
